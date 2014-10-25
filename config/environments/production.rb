9f69c153c097f2a4c341038d7c2edc4a9d995b71
Ilnur <i.kuchaev@softeam.co>
2014-10-25 19:38:02 +0600

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-alonsi::Application.configure do
+Alonsi::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
