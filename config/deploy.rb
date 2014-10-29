296fb8b23cf0672c6061f4b8fec5d029ea82cdf1
Ilnur <i.kuchaev@softeam.co>
2014-10-29 20:57:11 +0500

    asd

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ server "5.9.110.148", :web, :app, :db, primary: true
 
 set :application, "alonsi"
 set :user, "ilnur"
-set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_to, "/home/#{user}/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
