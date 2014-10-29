32d5fc1439d4b3a92e9ac69c46e86a7d911b4ea3
Ilnur <i.kuchaev@softeam.co>
2014-10-29 23:28:49 +0500

    asd

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,11 +2,11 @@ app = "alonsi"
 user = "ilnur"
 root = "/home/#{user}/#{app}/current"
 working_directory root
-pid "#{root}/tmp/pids/unicorn-site.pid"
-stderr_path "#{root}/log/unicorn-site.log"
-stdout_path "#{root}/log/unicorn-site.log"
+pid "#{root}/tmp/pids/unicorn-alonsi.pid"
+stderr_path "#{root}/log/unicorn-alonsi.log"
+stdout_path "#{root}/log/unicorn-alonsi.log"
 
-listen "/home/#{user}/#{app}/shared/unicorn-site.sock"
+listen "/home/#{user}/#{app}/shared/unicorn-alonsi.sock"
 # listen socket_file, :backlog => 1024
 worker_processes 2
 timeout 30
