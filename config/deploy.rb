bf87c5c201615f47be963b7d1947d464b513ce59
Ilnur <i.kuchaev@softeam.co>
2014-10-29 22:21:35 +0500

    asd

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,8 +14,6 @@ set :branch, "master"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-after "deploy", "deploy:cleanup" # keep only the last 5 releases
-
 namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
