f7dc43bd7e8441d76e87ca89e3b5cd9b6e012b7b
Ilnur <i.kuchaev@softeam.co>
2014-10-30 20:55:50 +0500

    asd

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,27 @@
 Rails.application.routes.draw do
 
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :sessions => "users/sessions" }
-  resources :users, :only => [:index]
+  # devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :sessions => "users/sessions" }
+  # resources :users, :only => [:index]
 
-  resources :tracks, only: [:index] do
-    collection do
-      get "/topcharts", to: "tracks#topcharts"
-      get "/random", to: "tracks#random"
-    end
-  end
+  # resources :tracks, only: [:index] do
+  #   collection do
+  #     get "/topcharts", to: "tracks#topcharts"
+  #     get "/random", to: "tracks#random"
+  #   end
+  # end
 
-  resources :tags, only: [:index] do
-    collection do
-      get "/tracks", to: "tags#tracks"
-    end
-  end
+  # resources :tags, only: [:index] do
+  #   collection do
+  #     get "/tracks", to: "tags#tracks"
+  #   end
+  # end
 
-  resources :playlists, only: [:index, :create, :show] do
-    collection do
-      post "/add", to: "playlists#create_track"
-    end
-  end
+  # resources :playlists, only: [:index, :create, :show] do
+  #   collection do
+  #     post "/add", to: "playlists#create_track"
+  #   end
+  # end
 
   root to: 'pages#index', id: 'home'
 
